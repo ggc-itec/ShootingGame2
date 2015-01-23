ccc8f3e915090adb446c9dde56a0e642d4d825ac
Derek Cofer <dcofer@ggc.edu>
2015-01-23 14:32:14 -0500

    The initial commit

---
+++ desktop/src/edu/ggc/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package edu.ggc.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import edu.ggc.game.ShootingGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ShootingGame(), config);
+	}
+}
