991f8d53cca7aa27373c390dc9d04666b45c0f3e
Derek Cofer <dcofer@ggc.edu>
2015-01-30 14:52:44 -0500

    Added menu with an exit button

--- core/src/edu/ggc/game/ShootingGame.java
+++ core/src/edu/ggc/game/ShootingGame.java
@@ -1,27 +1,44 @@
 package edu.ggc.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class ShootingGame extends ApplicationAdapter {
+import edu.ggc.game.screens.MainMenu;
+
+/**
+ * Changed the generated class to extend Game instead of ApplicationAdapter.
+ *
+ */
+public class ShootingGame extends Game {
+    //receives drawing commands 
 	SpriteBatch batch;
+	//reference to an image
 	Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		setScreen(new MainMenu());
 	}
-
-	@Override
+	
+	/**
+	 * Remove this method to delegate rendering to another Screen.
+	 */
+	
+	/*	
+    @Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	    //clear screen to RGBA
+		Gdx.gl.glClearColor(0, 1, 0, 1);
+		//Tells OpenGL which buffer to clear
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		//(0, 0) is from the bottom left of the screen
+		batch.draw(img, 100, 0);
 		batch.end();
-	}
+	}*/
 }
---
+++ core/src/edu/ggc/game/screens/MainMenu.java
@@ -0,0 +1,122 @@
+package edu.ggc.game.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+/**
+ * Represents the Main Menu for the game.
+ * 
+ * A Screen represents menu, settings, game screen
+ */
+public class MainMenu implements Screen
+{
+    //2D scene containing a hierarchy of actors
+    private Stage stage = new Stage();
+    
+    private Table table = new Table();
+    
+    //Resource for UI components
+    private Skin skin;
+    
+    private TextButton exitButton;
+        
+    public MainMenu()
+    {
+        setUpSkin();
+    }
+    
+    public void setUpSkin()
+    {
+        skin = new Skin();
+        
+        Pixmap pixmap = new Pixmap(150, 150, Format.RGBA8888);
+        pixmap.setColor(Color.WHITE);//Foreground color
+        pixmap.fill();
+        skin.add("white", new Texture(pixmap));
+        
+        BitmapFont font = new BitmapFont();
+        font.scale(1.5f);
+        skin.add("simple", font);
+        
+        TextButtonStyle textButtonStyle = new TextButtonStyle();
+        textButtonStyle.up = skin.newDrawable("white", Color.GREEN);//When this button shows up
+        textButtonStyle.down = skin.newDrawable("white", Color.YELLOW);//When the button is clicked and held
+        textButtonStyle.checked = skin.newDrawable("white", Color.BLUE);//After being clicked and released
+        textButtonStyle.over = skin.newDrawable("white", Color.RED);//When mouse hovers over it
+        textButtonStyle.font = skin.getFont("simple");
+        skin.add("simple", textButtonStyle);
+        
+        exitButton=new TextButton("Exit",textButtonStyle);
+        exitButton.setPosition(0, 0);
+    }
+    
+    @Override
+    public void show()
+    {
+        table.add(exitButton).row();//Add button to table
+        table.setFillParent(true);
+        stage.addActor(table);//Add table to stage
+        Gdx.input.setInputProcessor(stage);
+        exitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y)
+            {
+                Gdx.app.exit();
+            }
+        });
+    }
+
+    @Override
+    public void render(float delta)
+    {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act();
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height)
+    {
+    }
+
+    @Override
+    public void pause()
+    {
+    }
+
+    @Override
+    public void resume()
+    { 
+    }
+
+    @Override
+    public void hide()
+    {
+    }
+
+    @Override
+    public void dispose()
+    {
+        stage.dispose();
+        skin.dispose();
+    }
+
+}
