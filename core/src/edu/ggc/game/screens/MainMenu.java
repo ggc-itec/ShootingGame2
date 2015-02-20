a9573ec3032de63c40c18521ea9d8a9d2520880d
Derek Cofer <dcofer@ggc.edu>
2015-02-20 13:15:32 -0500

    Added JSON file

--- core/src/edu/ggc/game/screens/MainMenu.java
+++ core/src/edu/ggc/game/screens/MainMenu.java
@@ -3,21 +3,15 @@ package edu.ggc.game.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
-import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 /**
@@ -32,7 +26,6 @@ public class MainMenu implements Screen
     private Skin skin;
     private Table table;
     private TextButton exitButton;
-    private BitmapFont blackHoleFont;
     
     private Music menuMusic;
     
@@ -49,8 +42,7 @@ public class MainMenu implements Screen
         Gdx.input.setInputProcessor(stage);
         
         atlas = new TextureAtlas("ui/button.pack");
-        skin = new Skin(atlas);
-        blackHoleFont = new BitmapFont(Gdx.files.internal("font/blackhole.fnt"));
+        skin = new Skin(Gdx.files.internal("ui/menuSkin.json"), atlas);
         
         Image background = new Image(new Texture(Gdx.files.internal("background.jpg")));//TODO maybe a better way to display background
         
@@ -61,8 +53,7 @@ public class MainMenu implements Screen
         setUpButton();
         
         //create the Label to display the game name
-        LabelStyle labelStyle = new LabelStyle(blackHoleFont, Color.BLUE);
-        heading = new Label("Shooting Game 2", labelStyle);
+        heading = new Label("Shooting Game 2", skin);
         heading.setFontScale(2.0f);
         
         table.add(heading).spaceBottom(50).row();
@@ -76,16 +67,8 @@ public class MainMenu implements Screen
     }
     
     private void setUpButton()
-    {
-        TextButtonStyle textButtonStyle = new TextButtonStyle();
-        textButtonStyle.up = skin.getDrawable("button_up");//button_up is defined in ui/button.pack
-        textButtonStyle.down = skin.getDrawable("button_down");//button_down is defined in ui/button.pack
-        textButtonStyle.pressedOffsetX = 1;
-        textButtonStyle.pressedOffsetY = -1;
-        textButtonStyle.font = blackHoleFont;
-        textButtonStyle.fontColor = Color.BLUE;
-        
-        exitButton = new TextButton("Exit", textButtonStyle);
+    {        
+        exitButton = new TextButton("Exit", skin);
         exitButton.pad(10);//Padding around the button's text
         exitButton.addListener(new ClickListener() {
             @Override
@@ -99,8 +82,6 @@ public class MainMenu implements Screen
     @Override
     public void render(float delta)
     {
-//        Gdx.gl.glClearColor(0, 0, 0, 1);
-//        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act(delta);
         stage.draw();
         System.out.println("FPS: " + Gdx.graphics.getFramesPerSecond());
@@ -145,7 +126,6 @@ public class MainMenu implements Screen
         stage.dispose();
         atlas.dispose();
         skin.dispose();
-        blackHoleFont.dispose();
         menuMusic.dispose();
     }
 
